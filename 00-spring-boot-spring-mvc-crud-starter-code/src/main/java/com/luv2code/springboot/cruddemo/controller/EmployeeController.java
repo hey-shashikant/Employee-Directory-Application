ef0e6ce4d665321acc180967ed152ade64096b50
Shashikant Solanki <90645693+hey-shashikant@users.noreply.github.com>
2024-01-08 00:36:59 +0530

    added controller for getting all employees

---
+++ 00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/luv2code/springboot/cruddemo/controller/EmployeeController.java
@@ -0,0 +1,36 @@
+package com.luv2code.springboot.cruddemo.controller;
+
+import com.luv2code.springboot.cruddemo.entity.Employee;
+import com.luv2code.springboot.cruddemo.service.EmployeeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/employees")
+public class EmployeeController {
+    private EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService theEmployeeService) {
+        this.employeeService = theEmployeeService;
+    }
+
+    // add mapping for "/list"
+
+    @GetMapping("list")
+    public String listEmployees(Model theModel) {
+
+        // get the employees from db
+        List<Employee> theEmployes = employeeService.findAll();
+
+        // add to the spring model
+        theModel.addAttribute("employees",theEmployes);
+
+        return "list-employees";
+    }
+}
